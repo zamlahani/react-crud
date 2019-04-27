8efbb061fd90b5d9bcf772cf567f310bfb24036b
Mike Chabot <mike.chabot@live.com>
2019-04-27 15:40:52 -0400

    Fix tests

--- src/components/examples/router/IncrementRoute.jsx
+++ src/components/examples/router/IncrementRoute.jsx
@@ -3,6 +3,6 @@ import RouteContent from './RouteContent';
 
 import { increment } from '../../../redux/actions/action-creators';
 
-const IncrementRoute = () => <RouteContent icon="plus" label="Increment" action={increment} />;
+const IncrementRoute = () => <RouteContent icon="plus" path="/" label="Increment" action={increment} />;
 
 export default IncrementRoute;
